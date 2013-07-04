ffbf0d5b8aab560a3a09f5984d419bf2547b36f9
robknows <robmoore121@gmail.com>
2013-07-04 14:35:35 +0100

    Schrödinger's NullPointer was caused by java iterating too many times in a for each loop..... GJ JAVA. GJ.  It's gone now and the tests pass. gj me.

--- src/XMLDataFormatter.java
+++ src/XMLDataFormatter.java
@@ -1,2 +1,25 @@
 public class XMLDataFormatter {
+    public String makeTable(XMLAttribute[] attributes) {
+        String headerRow = makeHeaderRow(attributes);
+        String bodyRow = makeBodyRow(attributes);
+        return "<table>" + headerRow + bodyRow + "</table>";
+    }
+
+    public String makeHeaderRow(XMLAttribute[] attributes) {
+        String headerRow = "";
+        for (int i = 0; i < attributes.length - 1; i++) {
+            XMLAttribute attribute = attributes[i];
+            headerRow += "<th>" + attribute.name + "</th>";
+        }
+        return "<tr>" + headerRow + "</tr>";
+    }
+
+    public String makeBodyRow(XMLAttribute[] attributes) {
+        String bodyRow = "";
+        for (int i = 0; i < attributes.length - 1; i++) {
+            XMLAttribute attribute = attributes[i];
+            bodyRow += "<td>" + attribute.value + "</td>";
+        }
+        return "<tr>" + bodyRow + "</tr>";
+    }
 }
--- test/FormatterTest.java
+++ test/FormatterTest.java
@@ -4,4 +4,21 @@ import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class FormatterTest {
+    @Test
+    public void canMakeTableHeader() {
+        XMLDataFormatter formatter = new XMLDataFormatter();
+        XMLParser parser = new XMLParser();
+        XMLTag tag = parser.getXMLTagFromOpeningTag("<option name=\"http-port\" type=\"Integer\">");
+        assertThat(tag.attributes[0] == null, equalTo(false));
+        assertThat(tag.attributes[1] == null, equalTo(false));
+        assertThat(formatter.makeHeaderRow(tag.attributes), equalTo("<tr><th>name</th><th>type</th></tr>"));
+    }
+
+    @Test
+    public void canMakeTableCorrectly() {
+        XMLDataFormatter formatter = new XMLDataFormatter();
+        XMLParser parser = new XMLParser();
+        XMLTag tag = parser.getXMLTagFromOpeningTag("<option name=\"http-port\" type=\"Integer\">");
+        assertThat(formatter.makeTable(tag.attributes), equalTo("<table><tr><th>name</th><th>type</th></tr><tr><td>http-port</td><td>Integer</td></tr></table>"));
+    }
 }
