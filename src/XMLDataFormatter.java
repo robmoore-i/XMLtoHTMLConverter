ca4afe5fc15503591ced689343c8f248fc9ce0c5
robknows <robmoore121@gmail.com>
2013-07-04 14:00:58 +0100

    Restarting the formatting section of the code. The NullPointerException isn't budging and the debugger says that there is no NullPointer so there isn't much I can do but start again.

--- src/XMLDataFormatter.java
+++ src/XMLDataFormatter.java
@@ -1,22 +1,2 @@
 public class XMLDataFormatter {
-
-    public String makeTableFromXMLTag(XMLTag tag) {
-        return "<table border=\"1\">" + makeTableHeaderRow(tag) + makeTableBodyRow(tag) + "</table>";
-    }
-
-    private String makeTableHeaderRow(XMLTag tag) {
-        String tableRowData = "";
-        for (XMLAttribute attribute : tag.attributes) {
-            tableRowData = tableRowData.concat("<th>" + attribute.name + "</th>");
-        }
-        return "<tr>" + tableRowData + "</tr>";
-    }
-
-    private String makeTableBodyRow(XMLTag tag) {
-        String tableRowData ="";
-        for (XMLAttribute attribute : tag.attributes) {
-            tableRowData = tableRowData.concat("<td>" + attribute.value + "</td>");
-        }
-        return tableRowData;
-    }
 }
--- test/FormatterTest.java
+++ test/FormatterTest.java
@@ -4,24 +4,4 @@ import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class FormatterTest {
-    @Test
-    public void makesTableCorrectlyForSingleTag() {
-        XMLDataFormatter formatter = new XMLDataFormatter();
-        XMLParser parser = new XMLParser();
-        XMLTag tag = parser.getXMLTagFromOpeningTag("<option name=\"http-port\" type=\"Integer\" java=\"true\">");
-        assertThat(formatter.makeTableFromXMLTag(tag), equalTo(
-                "<table border=\"1\">" +
-                    "<tr>" +
-                        "<th>name</th>" +
-                        "<th>type</th>" +
-                        "<th>java</th>" +
-                    "</tr>" +
-                    "<tr>" +
-                        "<th>http-port</th>" +
-                        "<th>Integer</th>" +
-                        "<th>true</th>" +
-                    "</tr>" +
-                "</table>"
-        ));
-    }
 }
